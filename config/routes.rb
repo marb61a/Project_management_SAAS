f562c19630877f65624a6c13f8639829c35e80a6
marb <marb61a@gmail.com>
2016-07-26 12:59:53 +0000

    updating artifacts form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   
-
+  resources :user_projects
   resources :artifacts
-  resources :projects
+  resources :tenants do
+    resources :projects do
+      get 'users', on: :member
+      put 'add_user', on: :member
+    end
+  end
+  
+  
   resources :members
   get 'home/index'
 
@@ -20,6 +27,9 @@ Rails.application.routes.draw do
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
+  
+  match 'plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+  match 'plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
 
 
   # The priority is based upon order of creation: first created -> highest priority.
